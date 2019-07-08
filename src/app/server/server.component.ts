7388be857f64709018722406dee8d208bc12b5da
n3z1 <nezi.tajne520@gmail.com>
2019-07-08 19:55:34 +0200

    dodanie aktywacji przycisku po 2 sekundach

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -10,7 +10,7 @@ import { Component } from '@angular/core';
 export class ServerComponent 
 {
     // deklaracja zmiennych
-    server_id       : number = 10;
+    private server_id       : number = 10;
     server_status   : string = 'offline';
 
     // definicja funkcji 
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,14 +2,28 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
-  template:'<app-server></app-server><app-server></app-server><app-server></app-server>',  //templateUrl: './servers.component.html', // mozna uzyc odwołania do szablonu lub wkleić zawartość szablonu
+  // template:'<app-server></app-server><app-server></app-server><app-server></app-server>',  //templateUrl: './servers.component.html', // mozna uzyc odwołania do szablonu lub wkleić zawartość szablonu
+  templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
 })
 export class ServersComponent implements OnInit {
 
-  constructor() { }
+  // dodanie nowej zmiennej typu logicznego
+  allowNewServers : boolean = false;
+
+  constructor() 
+  {
+      // po 2 sekundach uruchom funkcje
+      // () => {} to to samo co wywołanie samej funkcji
+      // w {} podajemy instrukcje które mają się wykonać
+      setTimeout( () => {
+          this.allowNewServers = true;
+      } ,2000);
+
+  }
 
   ngOnInit() {
   }
+
 
 }
